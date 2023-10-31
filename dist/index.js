b4cd667646faedb934f062a70a85be3a21a1c36c
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-10-31 14:37:30 +0530

    TypeScript Configuration Done

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let age = 10;
+console.log(age);
+if (age < 50) {
+    age += 20;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 10;
+console.log(age);
+if (age < 50) {
+  age += 20;
+}
+console.log(age);
